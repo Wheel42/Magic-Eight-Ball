c4df87c18d9135b1f90f73aeea5b4341ce79a913
Wheel42 <145379003+Wheel42@users.noreply.github.com>
2023-11-29 10:52:41 -0700

    added the files
    Boom baby (throws old man out of window)

---
+++ main.js
@@ -0,0 +1,44 @@
+//Magic 8 Ball
+
+//  Button Event Listener
+document.getElementById("btn").addEventListener("click", BtnClicked);
+
+function BtnClicked() {
+  //Determine Input
+  let input = document.getElementById("input").value;
+  console.log(input);
+
+  if (input === "Does a magic 8 ball actually work?") {
+    ////Is the question preset 1?
+    console.log("Does a magic 8 ball actually work?");
+    document.getElementById("output").innerHTML = "How dare you doubt me!";
+  } else if (input === "Is JavaScript awesome?") {
+    //Is the question preset 2
+    console.log("Isjavaawesome");
+    document.getElementById("output").innerHTML = "Of Course!";
+  } else if (input === "") {
+    console.log("null");
+    document.getElementById("output").innerHTML = "Please ask a question...";
+  } else {
+    //You have discovered a new, mysterious element, Skyalnder. Perhaps I can tell you more about it... in time.
+    let randNum = Math.random();
+    console.log(randNum);
+    if (randNum < 0.2) {
+      console.log("1");
+      document.getElementById("output").innerHTML = "Without a Doubt.";
+    } else if (randNum < 0.4) {
+      console.log("2");
+      document.getElementById("output").innerHTML = "As I see it, yes.";
+    } else if (randNum < 0.6) {
+      console.log("3");
+      document.getElementById("output").innerHTML =
+        "Concentrate and ask again.";
+    } else if (randNum < 0.8) {
+      console.log("4");
+      document.getElementById("output").innerHTML = "Don't count on it.";
+    } else {
+      console.log("5");
+      document.getElementById("output").innerHTML = "Outlook not so good.";
+    }
+  }
+}
